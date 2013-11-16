b948b494e4fd6868df90121e503c4ca268251ffe
Luis Elizondo <lelizondo@gmail.com>
2013-11-16 16:53:58 -0600

    Added config.js file for tests

--- tests/project.js
+++ tests/project.js
@@ -1,10 +1,11 @@
 var should = require("should")
+	, config = require("./config")
 	, Drupal6 = require("../lib/drupal6");
 
 describe("Drupal6", function() {
 	it("should return an object", function(done) {
 
-
+		var data = config;
 
 		var project = new Drupal6(data);
 		project.cloneProject(function(err, stdout, stderr) {
